bee0efd7eedfa7cf5da964cec7ca75a4638270f3
bendered2332 <bendered@mail.uc.edu>
2021-09-12 15:35:43 -0400

    email script

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -3,5 +3,12 @@
 }
 
 $IP = getIP
-write-host("This machine's IP is $IP")
-write-host("This machine's IP is {0}" -f $IP)
+$USER = $env:USERNAME
+$NAME = $env:ComputerName
+$VERSION = $HOST.Version
+$DATE = Get-Date  -Format "dddd, MM/dd/yyyy"
+
+
+$Body = "This machine's IP is $IP. User is $User. Hostname is $NAME. PowerShell Version $VERSION. Today's Date is $DATE."
+write-host("$Body")
+Send-MailMessage -To "eddieb2332@gmail.com" -From "eddieb2332@gmail.com" -Subject "IT3038c" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL  -Credential (Get-Credential)
