55c2084ec3d47c9e76e502f631e93a2df0627ac1
Edward Bender <eddieb2332@gmail.com>
2021-10-08 15:32:32 -0400

    Server.js

--- labs/lab5.py
+++ labs/lab5.py
@@ -1,5 +1,5 @@
 # Take a word as input and count how many letters, how many vowels and how many consonants.
-
+# Check your script and make sure the latest changes are pushed. You're missing an important declaration near the top of your file for letter_Counter
 vowels = ['a', 'e', 'i', 'o', 'u']
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
