0f585f417f8d376c1da064762fce9835f1cfda42
Edward Bender <eddieb2332@gmail.com>
2021-10-21 13:30:00 -0400

    Module 7 code

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -11,4 +11,5 @@ $DATE = Get-Date  -Format "dddd, MM/dd/yyyy"
 
 $Body = "This machine's IP is $IP. User is $User. Hostname is $NAME. PowerShell Version $VERSION. Today's Date is $DATE."
 write-host("$Body")
-Send-MailMessage -To "eddieb2332@gmail.com" -From "eddieb2332@gmail.com" -Subject "IT3038c" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL  -Credential (Get-Credential)
+
+#Send-MailMessage -To "eddieb2332@gmail.com" -From "eddieb2332@gmail.com" -Subject "IT3038c" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL  -Credential (Get-Credential)
