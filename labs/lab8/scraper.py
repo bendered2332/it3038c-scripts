8e97b41187e8d3566b9100510d667f0a6f9ef027
Edward Bender <eddieb2332@gmail.com>
2021-10-31 14:57:16 -0400

    Added comments to code

--- labs/lab8/scraper.py
+++ labs/lab8/scraper.py
@@ -5,14 +5,13 @@ from bs4 import BeautifulSoup
 data = requests.get(
     "https://www.sparktrendz.com/products/aki-black-shorts").content
 soup = BeautifulSoup(data, 'html.parser')
-div = soup.find('span', class_='current_price').text.strip()
-#div = soup.find('div', class_="price__container price__container--display-price-true has-margin-right").text
 
+# Pulls the item cost and strips it of any whitespace
+price = soup.find('span', class_='current_price').text.strip()
+
+# Pulls the item name
 product = soup.find('h1', class_='product_name').text
 
-print(div)
-
-print(product)
-
+# Prints the item name and cost
 print("The item you are currently viewing is " +
-      product + "and it costs: " + str(div))
+      product + "and it costs: " + str(price))
