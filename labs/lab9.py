debb93993e48eb44aa11369f9aa0c47feba1f3a6
Edward Bender <eddieb2332@gmail.com>
2021-11-05 17:41:50 -0400

    finished lab 9

--- labs/lab9.py
+++ labs/lab9.py
@@ -4,4 +4,7 @@ import requests
 r = requests.get('http://localhost:3000')
 data = r.json()
 
-print(data)
+print(data[0]['name'] + " is " + data[0]['color'])
+print(data[1]['name'] + " is " + data[1]['color'])
+print(data[2]['name'] + " is " + data[2]['color'])
+print(data[3]['name'] + " is " + data[3]['color'])
